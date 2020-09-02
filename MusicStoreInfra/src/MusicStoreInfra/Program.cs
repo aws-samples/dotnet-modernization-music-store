8b2695922c42b937c9c60a191ab48e72865815d9
Vlad Hrybok <vgribok@hotmail.com>
2020-09-02 19:44:50 -0400

    Added CDK project deploying to ECS-Fargate + RDS SQL Server

---
+++ MusicStoreInfra/src/MusicStoreInfra/Program.cs
@@ -0,0 +1,27 @@
+﻿using Amazon.CDK;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace MusicStoreInfra
+{
+    sealed class Program
+    {
+        public static void Main(string[] args)
+        {
+            var app = new App();
+
+            Stack[] stacks = {
+                new BuildEnvStack(app),
+                new MusicStoreInfraStack(app)
+            };
+
+            var stackNames = stacks.Select(stack => stack.StackName);
+            string allStackNames = string.Join("\", \"", stackNames);
+
+            Console.WriteLine($"Stacks that can be deployed: \"{allStackNames}\".");
+
+            app.Synth();
+        }
+    }
+}
