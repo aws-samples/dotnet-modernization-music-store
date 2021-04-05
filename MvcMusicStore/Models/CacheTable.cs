00352bf7ee89d3bc63ccd44230d3bb27f89acc88
sagulati <sagulati@amazon.com>
2021-04-05 16:13:35 -0500

    changing music store to be stateless.

---
+++ MvcMusicStore/Models/CacheTable.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MvcMusicStore.Models
+{
+    [Table("MusicStoreCache")]
+    public class CacheTable
+    {
+        [Column(TypeName = "nvarchar(449)")]
+        [Required]
+        [Key]
+        [MaxLength(449)]
+        public string Id { get; set; }
+
+        [Column(TypeName = "varbinary(max)")]
+        [Required]
+        public byte[] Value { get; set; }
+        
+        public DateTimeOffset ExpiresAtTime { get; set; }
+        
+        public long? SlidingExpirationInSeconds { get; set; }
+        public DateTimeOffset? AbsoluteExpiration { get; set; }
+    }
+}
