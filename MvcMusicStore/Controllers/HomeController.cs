1fe4394e02c2d3038a4014ac0b84883edc38b72a
sagulati <sagulati@amazon.com>
2021-08-30 14:29:32 -0500

    removed Account, Checkout, ShoppingCart and Store functionality.

--- MvcMusicStore/Controllers/HomeController.cs
+++ MvcMusicStore/Controllers/HomeController.cs
@@ -26,7 +26,6 @@ namespace MvcMusicStore.Controllers
             // the albums with the highest count
 
             return storeDB.Albums
-                .OrderByDescending(a => a.OrderDetails.Count())
                 .Take(count)
                 .ToList();
         }
--- MvcMusicStore/Models/Album.cs
+++ MvcMusicStore/Models/Album.cs
@@ -32,6 +32,5 @@ namespace MvcMusicStore.Models
 
         public virtual Genre Genre { get; set; }
         public virtual Artist Artist { get; set; }
-        public virtual List<OrderDetail> OrderDetails { get; set; }
     }
 }
