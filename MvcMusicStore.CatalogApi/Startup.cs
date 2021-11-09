bfa82968884e2ad64d11d431f12a63dd9a7f8208
workshop-1c71e191-b6a2-4a53-8cf0-4fea15eb651b <user@codecommit.aws>
2021-11-08 18:16:43 -0800

    add default route

--- MvcMusicStore.CatalogApi/Startup.cs
+++ MvcMusicStore.CatalogApi/Startup.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
@@ -50,6 +51,10 @@ namespace MvcMusicStore.CatalogApi
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapControllers();
+                endpoints.MapGet("/", async context =>
+                {
+                    await context.Response.WriteAsync("Welcome to MusicStore Catalog API");
+                });
             });
         }
     }
