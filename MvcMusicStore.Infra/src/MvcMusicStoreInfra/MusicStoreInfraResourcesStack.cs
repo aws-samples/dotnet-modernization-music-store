3740533298bc576595d482d60a6ca017e5b72970
workshop-1c71e191-b6a2-4a53-8cf0-4fea15eb651b <user@codecommit.aws>
2021-11-08 17:58:06 -0800

    add the application infrastructure

--- MvcMusicStore.Infra/src/MvcMusicStoreInfra/MusicStoreInfraResourcesStack.cs
+++ MvcMusicStore.Infra/src/MvcMusicStoreInfra/MusicStoreInfraResourcesStack.cs
@@ -1,4 +1,12 @@
-﻿using Amazon.CDK;
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using Amazon.CDK;
+using Amazon.CDK.AWS.DynamoDB;
+using Amazon.CDK.AWS.EC2;
+using Amazon.CDK.AWS.Ecr.Assets;
+using Amazon.CDK.AWS.ECS;
+using Amazon.CDK.AWS.ECS.Patterns;
 
 namespace MvcMusicStoreInfra
 {
@@ -6,7 +14,50 @@ namespace MvcMusicStoreInfra
     {
         internal MusicStoreInfraResourcesStack(Construct scope, string id, IStackProps props = null) : base(scope, id, props)
         {
-            //You will create the Application Resources here.
+            //VPC
+            var vpc = new Vpc(this, "cluster-vpc", new VpcProps
+            {
+                Cidr = "172.51.0.0/16",
+                MaxAzs = 3
+            });
+
+            // Import DynamoDb Tables
+            var catalogTable = Table.FromTableArn(this, "imported-album-table", $"arn:aws:dynamodb:{this.Region}:{this.Account}:table/Catalog");
+
+            //ECR
+            //Build docker image and publish on ECR Repository
+            var asset = new DockerImageAsset(this, "web-api-docker-image", new DockerImageAssetProps
+            {
+                Directory = Path.Combine(Directory.GetCurrentDirectory(), "../MvcMusicStore.CatalogApi"),
+                File = "Dockerfile"
+            });
+
+            // Create ECS Cluster 
+            var cluster = new Cluster(this, "demo-cluster", new ClusterProps
+            {
+                Vpc = vpc,
+            });
+
+            //L3 Construct for ALB + ECS + Fargate
+            var loadBalancedFargateService = new ApplicationLoadBalancedFargateService(this, "demo-ecs-fargate-service", new ApplicationLoadBalancedFargateServiceProps
+            {
+                Cluster = cluster,
+                MemoryLimitMiB = 1024,
+                Cpu = 512,
+                TaskImageOptions = new ApplicationLoadBalancedTaskImageOptions
+                {
+                    Image = ContainerImage.FromDockerImageAsset(asset), 
+                    Environment = new Dictionary<string, string>()
+                        {
+                            {"AWS_REGION", this.Region},
+                            {"ASPNETCORE_ENVIRONMENT","Development"},
+                            {"ASPNETCORE_URLS","http://+:80"}
+                        }
+                }
+            });
+
+            //Grant Read Permission
+            catalogTable.GrantReadData(loadBalancedFargateService.Service.TaskDefinition.TaskRole);
         }
     }
 }
