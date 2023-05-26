86ba5fb9952c84e3b6c63729391d9b2d3ef7646f
ashdsi <63089530+ashdsi@users.noreply.github.com>
2023-05-26 12:56:44 +1000

    Update ExecuteUARatchetEvaluator.ps1

--- .github/workflows/ExecuteUARatchetEvaluator.ps1
+++ .github/workflows/ExecuteUARatchetEvaluator.ps1
@@ -78,11 +78,11 @@ function Evaluate-Results() {
      Generate-Analysis-Report -outputFileName "current_analysis_UA"
      Write-Host "Evaluating current analysis with master baseline"
      $q = Start-Process -FilePath .\evaluator\coderatchetingevaluator\CodeRatchetingEvaluator.exe -ArgumentList "parse --baseline .\evaluator\current_baseline_UA.json --compareWith .\report\current_analysis_UA.json --configFile .\evaluator\config\ratchet_config.yml --tool upgrade-assistant" -Wait -NoNewWindow -PassThru
-#      if($q.ExitCode)
-#      {
-#       Write-Error "Ratchet detected, Failing build..."
-#       Exit 1
-#      }
+      if($q.ExitCode)
+     {
+      Write-Error "Ratchet detected, Failing build..."
+       Exit 1
+      }
 }
 
 function Publish-Results-to-S3([string] $githash) {
